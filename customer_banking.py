b0aa50b0ee647cecdcf6b3c7edceec5766a25b62
ThomasClemons <taclemons@yahoo.com>
2023-11-13 22:17:26 -0500

    minor revisons to print statments

--- customer_banking.py
+++ customer_banking.py
@@ -17,8 +17,8 @@ def main():
     updated_savings_balance, interest_earned = create_savings_account(savings_balance, savings_interest, savings_maturity)
 
     # Print out the interest earned and updated savings account balance with interest earned for the given months.
-    print(f"\nUpdated Savings Account Balance is: ${updated_savings_balance:,.2f}")
-    print(f"\nInterest Earned Savings over {savings_maturity} months is: ${interest_earned:,.2f}\n")
+    print(f"\nSavings Account Balance is: ${updated_savings_balance:,.2f}")
+    print(f"\nSavings Account Interest Earned over {savings_maturity} months is: ${interest_earned:,.2f}\n")
 
     # Prompt the user to set the CD balance, interest rate, and months for the CD account.
     cd_balance = float(input("Please enter the CD account balance: "))
@@ -29,8 +29,8 @@ def main():
     updated_cd_balance, interest_earned = create_cd_account(cd_balance, cd_interest, cd_maturity)
 
     # Print out the interest earned and updated CD account balance with interest earned for the given months.
-    print(f"\nUpdated CD Account Balance is: ${updated_cd_balance:,.2f}")
-    print(f"\nInterest Earned on CD over {cd_maturity} months is: ${interest_earned:,.2f}\n")
+    print(f"\nCD Account Balance is: ${updated_cd_balance:,.2f}")
+    print(f"\nCD Account Interest Earned over {cd_maturity} months is: ${interest_earned:,.2f}\n")
 
 if __name__ == "__main__":
     # Call the main function.
